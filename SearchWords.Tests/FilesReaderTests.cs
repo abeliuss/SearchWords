0e71f4a506972e1d0004babd4d143759221a6323
LAPTOP-KQLMOAI1\Abel <abeliuss@gmail.com>
2020-03-06 06:09:51 +0100

    Start filesReader

---
+++ SearchWords.Tests/FilesReaderTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace SearchWords.Tests
+{
+    public class FilesReaderTests
+    {
+        [Fact]
+        public void When_Read_Files_Should_Count_Files_Read()
+        {
+            var filesReader = new FilesReader();
+
+        }
+    }
+}
